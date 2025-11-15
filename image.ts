aacd89c29bd33c8cfea64b094c929e7a4126ff10
Marina Nagashima <marinanagashima.ca@gmail.com>
2025-11-15 18:22:55 +0900

    Fix env import in image.ts

--- image.ts
+++ image.ts
@@ -1,7 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-import { dataset, projectId } from '../env'
+// 環境変数から直接読み込む
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
